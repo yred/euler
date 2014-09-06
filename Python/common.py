8948fe52cff23a10280576469dd0ce4cb6a31f36
Yasin A <yasin.ahakeem@gmail.com>
2014-09-06 02:40:09 -0700

    Added Python solutions to problems 12 & 13

--- Python/common.py
+++ Python/common.py
@@ -19,3 +19,15 @@ def primes():
     for n in count(3, 2):
         if is_prime(n):
             yield n
+
+
+def divisors(n):
+    """Returns the divisors of `n` as a sorted list"""
+    ds = set()
+
+    for i in range(1, int(sqrt(n) + 1)):
+        if n % i == 0:
+            ds.add(i)
+            ds.add(n/i)
+
+    return list(sorted(ds))
