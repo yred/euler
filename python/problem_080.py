6b17261f53ba95b81a539dba009662b4408c3265
Yasin A <yasin.ahakeem@gmail.com>
2014-09-24 23:31:48 -0700

    Added Python solution to problem 80

---
+++ python/problem_080.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Problem 80 - Square root digital expansion
+
+It is well known that if the square root of a natural number is not an integer,
+then it is irrational. The decimal expansion of such square roots is infinite
+without any repeating pattern at all.
+
+The square root of two is 1.41421356237309504880..., and the digital sum of the
+first one hundred decimal digits is 475.
+
+For the first one hundred natural numbers, find the total of the digital sums
+of the first one hundred decimal digits for all the irrational square roots.
+"""
+from decimal import Decimal, getcontext
+
+
+def non_squares(limit):
+    """
+    Returns all natural numbers up to and including `limit` that are not
+    perfect squares
+    """
+    for n in range(1, limit+1):
+        if int(n**0.5)**2 != n:
+            yield n
+
+
+def solution():
+    # Though a precision of 101 should be sufficient to retrieve the correct
+    # answer, the resulting sum is off by 1. 102 is the smallest precision that
+    # yields the correct answer
+    getcontext().prec = 102
+
+    return sum(sum(map(int, str(Decimal(n).sqrt()).replace('.', '')[:100]))
+               for n in non_squares(100))
+
+
+if __name__ == '__main__':
+    print(solution())
