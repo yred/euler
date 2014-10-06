c10e4b7daed02bb9edffcccdd95197c2083bbe52
Yasin A <yasin.ahakeem@gmail.com>
2014-10-06 00:47:13 -0700

    Improved the Python solution to problem 78

--- python/problem_078.py
+++ python/problem_078.py
@@ -18,31 +18,40 @@ Find the least value of n for which p(n) is divisible by one million.
 """
 from itertools import count
 
-from common import divisors, memoize
-
-
-@memoize
-def sumdivs(n):
-    """Returns the sum of the divisors of `n`"""
-    return sum(divisors(n))
-
 
 def solution():
     modulus = 10**6
 
+    # Initializing the list of the number of partitions: partitions[i] = p(i)
     partitions = [1, 1]
 
     for n in count(2):
-        # The number of partitions of n, p(n), is computed using the following
-        # formula:
-        #           n*p(n) = ∑ p(k)*(sum of divisors of n-k), for k = 0..n-1
+        # The number of partitions of n, p(n), is computed using the pentagonal
+        # number recursion formula:
         #
-        p_n = sum(partitions[i]*sumdivs(n-i) for i in range(n)) / n
+        #   P(n) = Σ(-1)^k+1 * [P(n - k(3k-1)/2) + P(n - k(3k+1)/2)]   (k=1..n)
+        #
+        # source:
+        #   http://www.had2know.com/academics/integer-partition-calculator.html
+        #
+        p_n = 0
+
+        for k in range(1, n+1):
+            fst = k*(3*k - 1)/2
+            snd = k*(3*k + 1)/2
+
+            if fst > n:
+                break
+
+            p_fst = partitions[n - fst]
+            p_snd = partitions[n - snd] if snd <= n else 0
+
+            p_n += ((-1)**(k+1))*(p_fst + p_snd)
 
         if (p_n % modulus) == 0:
             return n
         else:
-            partitions.append(p_n)
+            partitions.append(p_n % modulus)
 
 
 if __name__ == '__main__':
