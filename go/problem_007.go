dae674b797193510c29a61a8f8564c78f42017d3
Yasin A <yasin.ahakeem@gmail.com>
2014-10-13 23:33:08 -0700

    Added Go solution to problem 7

---
+++ go/problem_007.go
@@ -0,0 +1,35 @@
+// Problem 7 - 10001st prime
+//
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+// that the 6th prime is 13.
+//
+// What is the 10001st prime number?
+
+package main
+
+import (
+	"fmt"
+
+	"./common"
+)
+
+func main() {
+	fmt.Println(Solution())
+}
+
+func Solution() int {
+	// Index of the 10001st prime in a 0-indexed sequence
+	var target = 10000
+
+	var primes []int
+
+	for max := target; ; max *= 10 {
+		primes = common.PrimesUpTo(max)
+
+		if len(primes) >= target {
+			break
+		}
+	}
+
+	return primes[target]
+}
