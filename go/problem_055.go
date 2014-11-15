a9077ec8a236c55eed2826b5de261b3e0489d10a
Yasin A <yasin.ahakeem@gmail.com>
2014-11-14 23:26:40 -0800

    Added Go solution to problem 55

---
+++ go/problem_055.go
@@ -0,0 +1,73 @@
+// Problem 55 - Lychrel numbers
+//
+// If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+//
+// Not all numbers produce palindromes so quickly. For example,
+//
+//         349 + 943 = 1292,
+//         1292 + 2921 = 4213
+//         4213 + 3124 = 7337
+//
+// That is, 349 took three iterations to arrive at a palindrome.
+//
+// Although no one has proved it yet, it is thought that some numbers, like 196,
+// never produce a palindrome. A number that never forms a palindrome through the
+// reverse and add process is called a Lychrel number.
+//
+// Due to the theoretical nature of these numbers, and for the purpose of this
+// problem, we shall assume that a number is Lychrel until proven otherwise. In
+// addition you are given that for every number below ten-thousand, it will
+// either:
+//
+//     (i) become a palindrome in less than fifty iterations, or,
+//
+//     (ii) no one, with all the computing power that exists, has managed so far
+//     to map it to a palindrome. In fact, 10677 is the first number to be shown
+//     to require over fifty iterations before producing a palindrome:
+//
+//             4668731596684224866951378664 (53 iterations, 28-digits)
+//
+// Surprisingly, there are palindromic numbers that are themselves Lychrel
+// numbers; the first example is 4994.
+//
+// How many Lychrel numbers are there below ten-thousand?
+//
+// NOTE: Wording was modified slightly on 24 April 2007 to emphasise the
+// theoretical nature of Lychrel numbers.
+package main
+
+import (
+	"fmt"
+	"math/big"
+
+	"./common"
+)
+
+func main() {
+	fmt.Println(solution())
+}
+
+func solution() int {
+	limit := 10000
+	maxIterations := 50
+
+	// Assume that all numbers up to `limit` are Lychrel numbers
+	count := limit
+
+	for a := 1; a <= limit; a++ {
+		bigA := big.NewInt(int64(a))
+
+		for i := 1; i <= maxIterations; i++ {
+			revA, _ := big.NewInt(0).SetString(common.ReverseString(bigA.String()), 10)
+
+			bigA.Add(bigA, revA)
+
+			if common.IsPalindrome(bigA.String()) {
+				count--
+				break
+			}
+		}
+	}
+
+	return count
+}
