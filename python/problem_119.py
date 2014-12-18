f0f1be6b0b8d460f57054fbbde5a1828268e3732
Yasin A <yasin.ahakeem@gmail.com>
2014-12-17 23:57:50 -0800

    Minor update to the Python solution to problem 119

--- python/problem_119.py
+++ python/problem_119.py
@@ -3,11 +3,11 @@
 Problem 119 - Digit power sum
 
 The number 512 is interesting because it is equal to the sum of its digits
-raised to some power: 5 + 1 + 2 = 8, and 83 = 512. Another example of a number
-with this property is 614656 = 284.
+raised to some power: 5 + 1 + 2 = 8, and 8^3 = 512. Another example of a number
+with this property is 614656 = 28^4.
 
-We shall define an to be the nth term of this sequence and insist that a number
-must contain at least two digits to have a sum.
+We shall define a(n) to be the nth term of this sequence and insist that a
+number must contain at least two digits to have a sum.
 
 You are given that a(2) = 512 and a(10) = 614656.
 
