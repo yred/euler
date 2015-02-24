32c41c3de0c3372896fb782e4effbdfe5eb2b808
Yasin A <yasin.ahakeem@gmail.com>
2015-02-23 23:59:33 -0800

    Added Haskell solution to problem 4

---
+++ haskell/problem_004.hs
@@ -0,0 +1,14 @@
+-- Problem 4 - Largest palindrome product
+--
+-- A palindromic number reads the same both ways. The largest palindrome made
+-- from the product of two 2-digit numbers is 9009 = 91 × 99.
+--
+-- Find the largest palindrome made from the product of two 3-digit numbers.
+
+main = putStrLn $ show solution
+
+solution :: Integer
+solution = maximum [a*b | a <- [100..999], b <- [100..999], isPalindrome . show $ a*b]
+
+isPalindrome :: String -> Bool
+isPalindrome s = s == reverse s
