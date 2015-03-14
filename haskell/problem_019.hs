49b9589d00aae40094a7294e1c8459185c601732
Yasin A <yasin.ahakeem@gmail.com>
2015-03-13 23:58:43 -0700

    Initial work on Haskell solution to problem 19

---
+++ haskell/problem_019.hs
@@ -0,0 +1,29 @@
+-- Problem 19 - Counting Sundays
+--
+-- You are given the following information, but you may prefer to do some research
+-- for yourself.
+--
+--     - 1 Jan 1900 was a Monday.
+--
+--     - Thirty days has September,
+--       April, June and November.
+--       All the rest have thirty-one,
+--       Saving February alone,
+--       Which has twenty-eight, rain or shine.
+--       And on leap years, twenty-nine.
+--
+--     - A leap year occurs on any year evenly divisible by 4, but not on a
+--       century unless it is divisible by 400.
+--
+-- How many Sundays fell on the first of the month during the twentieth century
+-- (1 Jan 1901 to 31 Dec 2000)?
+import Data.Time.Calendar
+import Data.Time.Calendar.WeekDate
+
+main = putStrLn $ show solution
+
+solution :: Int
+solution = length $ filter (\(_, _, weekDay) -> weekDay == 7) $ map toWeekDate firstDays
+
+firstDays :: [Day]
+firstDays = zipWith (\year month -> fromGregorian year month 1) [1901..2000] [1..12] 
