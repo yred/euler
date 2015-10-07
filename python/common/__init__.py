525f2d862f3b57695646a7570c7e0099652cc64b
Yasin A <yasin.ahakeem@gmail.com>
2015-10-06 22:58:09 -0700

    Minor corrections to multicount() helper

--- python/common/__init__.py
+++ python/common/__init__.py
@@ -149,13 +149,13 @@ def multicount(start, steps):
     A modified version of itertools.count() that adds support for unevenly
     spaced values
 
-    >>> zip(range(7), multicount(10, 1, 3, 10))
+    >>> zip(range(7), multicount(10, [1, 3, 10]))
     [(0, 10), (1, 11), (2, 13), (3, 20), (4, 21), (5, 23), (6, 30)]
 
-    >>> zip(range(5), multicount(100, 25, 100))
+    >>> zip(range(5), multicount(100, [25, 100]))
     [(0, 100), (1, 125), (2, 200), (3, 225), (4, 300)]
     """
-    steps = sorted(steps) or [1]
+    steps = sorted(steps)
 
     for n in count(start, steps[-1]):
         yield n
