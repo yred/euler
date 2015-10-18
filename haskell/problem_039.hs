8541ebf587689ae68f8d55dcf04503e3db72e4ad
Yasin A <yasin.ahakeem@gmail.com>
2015-10-17 23:18:52 -0700

    Added Haskell solution to problem 39

---
+++ haskell/problem_039.hs
@@ -0,0 +1,21 @@
+-- Problem 39 - Integer right triangles
+--
+-- If p is the perimeter of a right angle triangle with integral length sides,
+-- {a,b,c}, there are exactly three solutions for p = 120:
+--
+--         {20, 48, 52}, {24, 45, 51}, {30, 40, 50}
+--
+-- For which value of p ≤ 1000, is the number of solutions maximised?
+import Common.Utils
+
+
+main = putStrLn $ show solution
+
+solution :: Int
+solution = snd . maximum . map (\p -> (length $ rightTriangles p, p)) $ [4..1000]
+
+rightTriangles :: Int -> [(Int, Int, Int)]
+rightTriangles p = [(a, b, c) | a <- [1..maxA], b <- [(a + 1)..(maxB a)], c <- [p - a - b], a*a + b*b == c*c]
+    where
+        maxA = p `div` 2
+        maxB a = (p - a) `div` 2
