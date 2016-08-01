c97a3aad1aea69b981bddfdc95b49f37e916a67a
Yasin A <yasin.ahakeem@gmail.com>
2016-07-31 23:54:36 -0700

    Added Rust solution to problem 35

---
+++ rust/problem_035.rs
@@ -0,0 +1,101 @@
+// Problem 35 - Circular primes
+//
+// The number, 197, is called a circular prime because all rotations of the
+// digits: 197, 971, and 719, are themselves prime.
+//
+// There are thirteen such primes below 100:
+//
+//     2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+//
+// How many circular primes are there below one million?
+use std::collections::HashSet;
+
+fn main() {
+    println!("{}", solution());
+}
+
+struct Primes {
+    previous: Vec<usize>,
+}
+
+impl Primes {
+    fn new() -> Primes {
+        Primes{ previous: Vec::new() }
+    }
+}
+
+impl Iterator for Primes {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        let start = match self.previous.last() {
+            Some(&n) => n+1,
+            None     => 2,
+        };
+
+        let mut next_prime = 0;
+
+        for n in start.. {
+            let isqrn = (n as f64).sqrt().floor() as usize;
+
+            if self.previous.iter().take_while(|k| **k <= isqrn).all(|k| n%k > 0) {
+                next_prime = n;
+                break;
+            }
+        }
+
+        self.previous.push(next_prime);
+        Some(next_prime)
+    }
+}
+
+fn solution() -> usize {
+    let plimit = 1_000_000;
+    let primes = Primes::new().take_while(|&p| p < plimit)
+                              .collect::<Vec<usize>>();
+
+    let mut primeset = HashSet::new();
+    let mut circulars = Vec::new();
+
+    for p in primes {
+        primeset.insert(p);
+
+        let mut is_circular = true;
+        let pgroup = rotations(p);
+
+        for &n in &pgroup {
+            if !primeset.contains(&n) {
+                is_circular = false;
+                break;
+            }
+        }
+
+        if !is_circular {
+            continue;
+        }
+
+        circulars.push(p);
+        circulars.extend(pgroup.iter());
+    }
+
+    circulars.len()
+}
+
+fn rotations(n: usize) -> Vec<usize> {
+    let mut results = Vec::new();
+
+    let pow_10 = 10usize.pow((n.to_string().len() - 1) as u32);
+    let mut _n = n;
+
+    while true {
+        _n = (_n % 10)*pow_10 + (_n / 10);
+
+        if _n != n {
+            results.push(_n);
+        } else {
+            break;
+        }
+    }
+
+    results
+}
