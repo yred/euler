61050cf4749dfedfb73cbfb2eebd20dd63ad0ecb
Yasin A <yasin.ahakeem@gmail.com>
2016-08-01 23:42:35 -0700

    Minor refactor

--- rust/problem_035.rs
+++ rust/problem_035.rs
@@ -10,43 +10,11 @@
 // How many circular primes are there below one million?
 use std::collections::HashSet;
 
+mod common;
+use common::primes::Primes;
+
 fn main() {
     println!("{}", solution());
-}
-
-struct Primes {
-    previous: Vec<usize>,
-}
-
-impl Primes {
-    fn new() -> Primes {
-        Primes{ previous: Vec::new() }
-    }
-}
-
-impl Iterator for Primes {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<usize> {
-        let start = match self.previous.last() {
-            Some(&n) => n+1,
-            None     => 2,
-        };
-
-        let mut next_prime = 0;
-
-        for n in start.. {
-            let isqrn = (n as f64).sqrt().floor() as usize;
-
-            if self.previous.iter().take_while(|k| **k <= isqrn).all(|k| n%k > 0) {
-                next_prime = n;
-                break;
-            }
-        }
-
-        self.previous.push(next_prime);
-        Some(next_prime)
-    }
 }
 
 fn solution() -> usize {
